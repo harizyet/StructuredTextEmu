becc715af08a39a02597c196a2ff2a5a25ebdae5
Hariz <harizyet@gmail.com>
2022-07-30 15:15:48 +0800

    Added Project. Added File Reader Added Test Script

---
+++ TextReader/FunctionClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StructuredTextEmu.TextReader
+{
+	public enum FunctionType
+	{
+		IfThen,
+		IfElse,
+		Function
+	}
+
+	public class FunctionClass
+	{
+		public FunctionType FunctionType;
+		public Dictionary<string,string> VariablesDeclared = new Dictionary<string,string>();
+	}
+}
