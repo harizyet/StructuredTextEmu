e63301d5fedca1720f4c0b5eea2b7ac2a4f2a9d7
Hariz Yet <hariz.yet@beca.com>
2022-08-02 12:07:09 +0800

    Line Interpreter

---
+++ Interpreter/LineInterpreter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using StructuredTextEmu.TextReader;
+
+namespace StructuredTextEmu.Interpreter
+{
+    public class LineInterpreter
+    {
+        public FunctionClass Interprete(string line)
+        {
+            for (int i = 0; i < ConstReservedKeywords.Keywords.Length; ++i)
+            {
+                if (line.Contains(ConstReservedKeywords.Keywords[i]))
+                {
+                    switch (ConstReservedKeywords.Keywords[i])
+                    {
+                        case ConstReservedKeywords.Set:
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ TextReader/ConstReservedKeywords.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace StructuredTextEmu.TextReader
+{
+    public class ConstReservedKeywords
+    {
+        public const string Keyword_IF = "IF";
+        public const string Keyword_ELSE = "ELSE";
+        public const string Keyword_THEN = "THEN";
+        public const string Keyword_END_IF = "END_IF";
+        public const string Keyword_AND = "AND";
+        public const string Keyword_OR = "OR";
+        public const string Set = ":=";
+
+        public const string Delimiter = ";";
+        public const string Comment_Start = "(*";
+        public const string Comment_End = "*)";
+
+        public static string[] Keywords = new string[]
+        {
+            Keyword_AND,
+            Keyword_OR,
+            Keyword_END_IF,
+            Keyword_IF,
+            Keyword_ELSE,
+            Keyword_THEN,
+            Set
+        };
+    }
+}
+
